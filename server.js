6f0b3a475425366c7e8895797bd138db83dc277b
sekarvaradha <segarvaradha@gmail.com>
2016-09-21 11:40:47 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,6 +26,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 app.get('/ui/1.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '1.png'));
 });
