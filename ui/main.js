f54bfed8f079c54c128326b90663bd545f1f97a9
sekarvaradha <segarvaradha@gmail.com>
2016-09-28 17:06:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,13 @@
 // counter code
-var button=Document.getElementById("counter");
-button.onClick =function(){
+var button=document.getElementById("counter");
+button.onClick = function(){
 // make a request to counter endpoint
 var request = new XMLHttpRequest();
 // capture the response and store in a variable
 request.onreadystatechange =function(){
-  if (request.readystate === XMLHttpRequest.DONE)
+  if (request.readyState === XMLHttpRequest.DONE)
     {
-      if (request.Status==200)
+      if (request.Status===200)
        { 
 	var counter=request.responseText;
 	var span = document.getElementById('count');
@@ -17,6 +17,6 @@ request.onreadystatechange =function(){
    
 };
 // render the variable in the correct span
-  request.open('GET', 'https://sekarvaradha.imad.hasura-app.io/counter','true');
+  request.open('GET', 'http://sekarvaradha.imad.hasura-app.io/counter','true');
   request.send(null);
 };
