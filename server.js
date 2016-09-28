457e76a05a3fb2cdfcea1823c59a5e5773f64c59
sekarvaradha <segarvaradha@gmail.com>
2016-09-28 16:55:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ res.send(counter.toString());
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-
-
-
 app.get('/article-one', function (req, res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
    });
