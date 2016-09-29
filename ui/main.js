23626dc8b973f1f2efb9b72918238421031ee663
sekarvaradha <segarvaradha@gmail.com>
2016-09-29 21:56:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,22 @@ var button=document.getElementById("counter");
 
 button.onclick = function(){
  
-  counter=counter+1;
-  var span=document.getElementById("count");
-  span.innerHTML=counter.toString();
-  
+ var request = new XMLHttpRequest();
+    //create a request
+ request.onreadystatechange =function(){
+ if (request.readyState === XMLHttpRequest.DONE)
+  {
+      // take some action
+    if (request.Status===200)
+       { 
+        counter=counter+1;
+        var span=document.getElementById("count");
+         span.innerHTML=counter.toString();
+       }
+  }
 };
 
+   // make the request
+    request.open('GET', 'http://sekarvaradha.imad.hasura-app.io/counter' +name,true);
+     request.send(null);
+};
