2e6df16fc18a22a8cd1c643d9201fd8d0e65359b
sekarvaradha <segarvaradha@gmail.com>
2016-09-29 22:43:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,35 @@ button.onclick = function(){
     request.open('GET', 'http://sekarvaradha.imad.hasura-app.io/counter', true);
      request.send(null);
 };
+
+// Name List Programme
+
+var submit =document.getElementById('submit-btn');
+ submit.onclick = function() {
+  var request = new XMLHttpRequest();
+// capture the response and store in a variable
+  request.onreadystatechange =function(){
+     if (request.readyState === XMLHttpRequest.DONE)
+      {
+      if (request.Status===200)
+       { 
+	     var names=request.responseText;
+	     var list='';
+        for (var i=0;i<name.length;i++)
+          {
+           list += '<li>' + names[i] + '</li>';
+             }
+	
+	     var ul = document.getElementById(namelist);
+         ui.innerHTML =list;
+         }
+   }        
+  
+ 
+  
+};
+    var nameinput=document.getElementbyId('Name');
+    var name=nameinput.value;
+    request.open('GET', 'http://sekarvaradha.imad.hasura-app.io/submit-name?name='+name,true);
+     request.send(null);
+ };  
