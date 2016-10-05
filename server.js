1850b0649c803a0b6f75b175a6d2085895d9d314
sekarvaradha <segarvaradha@gmail.com>
2016-10-05 07:02:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,29 +61,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/back', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
 
 app.get('/article-one', function (req, res){
    res.send(createTemplate(articleone));
    });
    
-app.get('/article-two', function (req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-   });
-   
-app.get('/article-three', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-   });
-   
-app.get('/home', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'home.html'));
-   });
-   
-   app.get('/profile', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
-   });
+
    
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
