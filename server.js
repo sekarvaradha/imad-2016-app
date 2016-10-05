9217f5a68973da9a1887d47ceca1b6426ad4eef0
sekarvaradha <segarvaradha@gmail.com>
2016-10-05 19:26:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,8 @@ app.get('/', function (req, res) {
 
 
 app.get('/articleName', function (req, res){
-   res.send(createTemplate(articles(articleName)));
+    var articleName=req.params.articleName;
+ res.send(createTemplate(articles(articleName)));
    });
    
 
