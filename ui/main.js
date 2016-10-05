0035df3cdf15f0d329e95e89e9692d20c3af24e2
sekarvaradha <segarvaradha@gmail.com>
2016-10-05 22:49:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -77,6 +77,7 @@ var submit =document.getElementById('submit-btn');
           
         var comment =request.responseText;
         var div =document.getElementById("show-comment");
+         div.style.text-align='center';
          div.innerHTML= comment;
        }
   }
