797edd684f19b9358e3c034c0b7c70ec53c6a4f5
sekarvaradha <segarvaradha@gmail.com>
2016-10-08 00:08:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,10 +123,10 @@ app.get('/ui/article.js',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article.js'));
  });
 
-app.get('/:articleName', function (req, res){
+/*app.get('/:articleName', function (req, res){
     var articleName=req.params.articleName;
  res.send(createTemplate(articles[articleName]));
-   }); 
+   }); */
    
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
