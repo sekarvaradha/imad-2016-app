002d01fe54e8d8fdfa740e6b581c0384c5e99a5b
sekarvaradha <segarvaradha@gmail.com>
2016-10-08 00:07:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
+/*var articles = {
  'article-one' :{
   title:'Article 1 ! SEGAR',
   heading:'Article-one',
@@ -37,9 +37,9 @@ var articles = {
     <p> This is my third article. 
     </p>`
      }
-};
+};*/
 
-function createTemplate (data) {
+/*function createTemplate (data) {
      var title= data.title;
      var date=data.date;
      var heading=data.heading;
@@ -85,7 +85,7 @@ var HTMLTemplate =`
         <script src="ui/main.js"></script>
         </html>`;
 return HTMLTemplate;
-}
+}*/
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
