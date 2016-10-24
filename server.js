a979f12bade1c5c84eadfc4992c02a1d65f80584
sekarvaradha <segarvaradha@gmail.com>
2016-10-24 17:15:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/test-db", function (req,res){
     // make a select request
     // return a response with the result
     
-    pool.query("SELECT * FROM test",function (err, result){
+    pool.query("SELECT * FROM article",function (err, result){
        if (err){
            res.status(500).send (err.toString());
           } else {
