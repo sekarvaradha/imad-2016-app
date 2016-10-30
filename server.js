4490468291925b482d931e3a96013873b3254012
sekarvaradha <segarvaradha@gmail.com>
2016-10-30 13:58:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -177,7 +177,7 @@ res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 
 app.get('/articles/:articleName', function (req, res){
 //var articleName=req.params.articleName;
-pool.query("SELECT * FROM article WHERE title= '"+req.params.articleName+"'", function(err,result)){
+pool.query("SELECT * FROM article WHERE title="+req.params.articleName, function(err,result){
     if(err){
         res.status(500).send(error.toString());
         } 
