f90fc6bb062bba70871ecfb961e891178c286c9b
sekarvaradha <segarvaradha@gmail.com>
2016-11-03 23:12:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/login-test", function (req,res){
     // make a select request
     // return a response with the result
     
-    pool.query("SELECT * FROM login where username='"+req.params.username +"'", function(err,result){
+    pool.query("SELECT * FROM login WHERE username='"+req.params.username +"'", function(err,result){
        if (err){
            res.status(500).send (err.toString());
           } else {
