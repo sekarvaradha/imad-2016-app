d79cb5a9591a25bbee7d30011ee0590b690ed4e7
sekarvaradha <segarvaradha@gmail.com>
2016-11-09 15:48:34 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -226,8 +226,7 @@ function hash(input,salt){
  }
 
 
-app.get('hash/:input',function(req,res){
-    
+app.get('hash/:input', function(req,res){
     var hashedstring=hash(req.params.input,"this is some random string");
     res.send(hashedstring);
 });
