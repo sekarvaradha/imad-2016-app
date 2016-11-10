41395c9fe41df6364c50c23352395e0a3f117c16
sekarvaradha <segarvaradha@gmail.com>
2016-11-10 01:04:16 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -216,9 +216,9 @@ app.get('/hash/:input', function(req,res){
     res.send(hashedstring);
 });
 
-app.get('/createuser/:username', function(req,res){
+app.get('/createuser/:username/:password', function(req,res){
 //username, password
-var salt=crypto.getrandomBytes(128).toString('hex');
+var salt=crypto.randomBytes(128).toString('hex');
 var dbString= hash(input,salt);
 pool.query('INSERT INTO login(username,password) VALUES ($1,$2)',[username,dbString], function(err,result){
     if (err) {
