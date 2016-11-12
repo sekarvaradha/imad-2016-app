03a123c75d307db0136f2474f6ee510b5f01322c
sekarvaradha <segarvaradha@gmail.com>
2016-11-12 12:13:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -137,7 +137,7 @@ var request = new XMLHttpRequest();
 
 request.open('POST', 'http://sekarvaradha.imad.hasura-app.io/login', true);
 //request.open('POST', 'http://localhost:8080/login', true);
-request.setRequestHeader('Content-Type','application/json');
+//request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({username:username,password:password})); 
 }; 
 }
