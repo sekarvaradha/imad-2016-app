607af324dcdcf1031f64ad3f2710e720441caf21
sekarvaradha <segarvaradha@gmail.com>
2016-11-29 06:29:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -160,7 +160,7 @@ var password=document.getElementById('password').value;
 console.log(username);
 console.log(password);
 
-request.open('POST', '/login ', true);
+request.open('POST', '/login', true);
 request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({username: username, password: password})); 
 }; 
